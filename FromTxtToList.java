14e16a732ec5a4133408ef6145102812532f06a3
Armen <armenrt@yahoo.com>
2016-11-16 12:55:08 +0400

    modified in proucess for game menu

--- FromTxtToList.java
+++ FromTxtToList.java
@@ -8,23 +8,11 @@ public class FromTxtToList {
 	public List<String> linesMenuText = new ArrayList<String>();
 
 	public void readMenuFromFile() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader("C:/myjava/Otherprojects/eckelexsamples/res/MenuText"));
+		BufferedReader reader = new BufferedReader(
+				new FileReader("C:/myjava/Otherprojects/eckelexsamples/res/MenuText"));
 		while ((line = reader.readLine()) != null) {
 			linesMenuText.add(line);
 		}
 		reader.close();
 	}
-
-//	public static void main(String[] args) {
-//		FromTxtToList txtToList = new FromTxtToList();
-//		try {
-//			txtToList.readMenuFromFile();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		for (String st : linesMenuText) {
-//			System.out.println(st.toString());
-//		}
-//	}
 }
--- TreeForMenu.java
+++ TreeForMenu.java
@@ -1,13 +1,14 @@
 package menutree;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class TreeForMenu extends Tree<String> {
-	String line = null;
-	String[] linePartsDoubleDot;
 
-	public void generedMenuItems() {
+	public TreeForMenu generateMenuLevels() {
+		TreeForMenu tfm = new TreeForMenu();
 		FromTxtToList fttl = new FromTxtToList();
 		try {
 			fttl.readMenuFromFile();
@@ -16,36 +17,60 @@ public class TreeForMenu extends Tree<String> {
 		}
 
 		for (int i = 0; i < fttl.linesMenuText.size(); i++) {
-			line = (String) fttl.linesMenuText.get(i);
+			String line = (String) fttl.linesMenuText.get(i);
 			if (line.length() != 0) {
-				linePartsDoubleDot = line.split(":");
-				String[] level = linePartsDoubleDot[0].split(Pattern.quote("."));
-				Node<String> node = new Node(linePartsDoubleDot[1]);
+				String[] linePartsSplitedByDD = line.split(":");
+				String levels = linePartsSplitedByDD[0];
+				if(linePartsSplitedByDD.length != 1) {
+				String content = linePartsSplitedByDD[1];
+				String[] level = levels.split(Pattern.quote("."));
+				Node<String> node = new Node<String>(content);
 				if (level.length == 1) {
-					this.getRootNode().addChildNode(node);
-					System.out.println(level[0] + ". " + this.getRootNode().getChildNode(Integer.parseInt(level[0])).getContent());
-				} 
-					if (level.length == 2) {
-						this.getRootNode().getChildNode(Integer.parseInt(level[0])).addChildNode(node);
-						System.out.println("\t" + level[1] + ". " +this.getRootNode()
-								.getChildNode(Integer.parseInt(level[0]))
-								.getChildNode(Integer.parseInt(level[1])).getContent());
-					}
-						if(level.length == 3) {
-					this.getRootNode().getChildNode(Integer.parseInt(level[0])).getChildNode(Integer.parseInt(level[1]))
+					tfm.getRootNode().addChildNode(node);
+//					return this.getRootNode().getChildren();
+//					System.out.println(
+//							level[0] + ". " + this.getRootNode()
+//									.getChildNode(Integer.parseInt(level[0])).getContent());
+				}
+				if (level.length == 2) {
+					tfm.getRootNode().getChildNode(Integer.parseInt(level[0])).addChildNode(node);
+//					return this.getRootNode().getChildNode(Integer.parseInt(level[0])).getChildren();
+//					System.out.println(
+//							"\t" + level[1] + ". " + this.getRootNode()
+//									.getChildNode(Integer.parseInt(level[0]))
+//									.getChildNode(Integer.parseInt(level[1])).getContent());
+				}
+				if (level.length == 3) {
+					tfm.getRootNode().getChildNode(Integer.parseInt(level[0])).getChildNode(Integer.parseInt(level[1]))
 							.addChildNode(node);
-					System.out.println("\t\t" + level[2] + ". " + this.getRootNode()
-							.getChildNode(Integer.parseInt(level[0]))
-							.getChildNode(Integer.parseInt(level[1]))
-							.getChildNode(Integer.parseInt(level[2])).getContent());
+//					return this.getRootNode().getChildNode(Integer.parseInt(level[0])).getChildNode(Integer.parseInt(level[1]))
+//							.getChildren();
+//					System.out.println(
+//							"\t\t" + level[2] + ". " + this.getRootNode()
+//									.getChildNode(Integer.parseInt(level[0]))
+//									.getChildNode(Integer.parseInt(level[1]))
+//									.getChildNode(Integer.parseInt(level[2])).getContent());
 				}
 			}
 		}
+		}
+		return tfm;
 	}
 
+	public void generateMenuForGame() throws IOException {
+		TreeForMenu mfg;
+		Map<Integer, Node<String>> menuLevel1, menuLevel2, menuLevel3;
+		mfg = generateMenuLevels();
+		menuLevel1 = mfg.getRootNode().getChildren();
+		for(Integer i: menuLevel1.keySet())
+			System.out.println(i + ". " + menuLevel1.get(i).getContent());
+		int i = System.in.read();
+//		menuLevel1.
+	}
+	
 	public static void main(String[] args) {
 		TreeForMenu tfm = new TreeForMenu();
 
-		tfm.generedMenuItems();
+		tfm.generateMenuForGame();
 	}
 }
